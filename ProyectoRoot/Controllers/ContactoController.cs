d8ba5917270818572ce0bdd9f67e3451b1d00f6b
Luii Navas <luish.navarretes@gmail.com>
2023-03-07 17:38:49 -0300

    Mostrando lista de Contactos en bruto.

---
+++ ProyectoRoot/Controllers/ContactoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoRoot.Controllers.Models;
+using System.Collections.Generic;
+
+namespace ProyectoRoot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+        private List<Contacto> listContacto = new List<Contacto> {
+        new Contacto { Id=1,Nombre="Luis",Apellido="Navarrete",Apodo="Luii",Ubicacion="Chile" },new Contacto { Id=2,Nombre="Ana",Apellido="Sánchez",Apodo="Ani",Ubicacion="Santiago" } };
+
+        // GET: api/<ContactoController>
+        [HttpGet]
+        public IEnumerable<Contacto> Get()
+        {
+            return listContacto;
+        }
+
+        // GET api/<ContactoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ContactoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContactoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContactoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
