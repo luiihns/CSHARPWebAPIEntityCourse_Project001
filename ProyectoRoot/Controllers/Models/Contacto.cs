35eb9cdc9e9b79be1ad041844de24009642b055e
Luii Navas <luish.navarretes@gmail.com>
2023-03-07 16:56:40 -0300

    Primer Modelo.

---
+++ ProyectoRoot/Controllers/Models/Contacto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProyectoRoot.Controllers.Models
+{
+    public class Contacto
+    {
+        public int Id{ get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Apodo { get; set; }
+        public string Ubicacion { get; set; }
+        public DateTime FechaCreacion{ get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
